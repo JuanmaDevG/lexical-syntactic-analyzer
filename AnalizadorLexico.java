f80510a6926d65598c2b491fdc8dd527c027c329
JuanmaDevG <juanmagonzalezmartinez@protonmail.com>
2026-06-23 16:50:45 +0200

    Minimal changes because I don't know the theory yet

---
+++ AnalizadorLexico.java
@@ -0,0 +1,9 @@
+import java.io.RandomAccessFile;
+
+public class AnalizadorLexico {
+  private RandomAccessFile f; // TODO: probably remove
+
+  public AnalizadorLexico(RandomAccessFile f) {
+    this.f = f;
+  }
+}
---
+++ AnalizadorSintacticoDR.java
@@ -0,0 +1,15 @@
+import java.io.RandomAccessFile;
+
+public class AnalizadorSintacticoDR {
+  private AnalizadorLexico al;
+
+  public AnalizadorSintacticoDR(AnalizadorLexico al) {
+    this.al = al;
+  }
+
+  public void S() {
+  }
+
+  public void comprobarFinFichero() {
+  }
+}
