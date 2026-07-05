0bb7910f015afc8dc24fbe150d8d00525e3eab01
JuanmaDevG <juanmagonzalezmartinez@protonmail.com>
2026-07-06 01:19:10 +0200

    adding the state machine behaviour to the lexer

--- AnalizadorLexico.java
+++ AnalizadorLexico.java
@@ -1,99 +1,93 @@
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.util.Iterator;
+import java.lang.StringBuilder;
 
 public class AnalizadorLexico {
-  private RandomAccessFile f;
+  private RandomAccessFile file;
   private int fila, columna;
+  private int lastType;
+  private int lastTkLen;
 
-  public AnalizadorLexico(RandomAccessFile f) {
-    this.f = f;
+  private static final int SKIP = 0, FINAL = -1, ERROR = -2;
+
+  public AnalizadorLexico(RandomAccessFile file) {
+    this.file = file;
     fila = 0;
     columna = 0;
+    lastTkLen = 0;
   }
 
   public Token siguienteToken() {
-    char curChar;
-    int tipo = Token.EOF;
-    Iterator<RandomAccessFile> front, back;
+    StringBuilder lexbuilder = new StringBuilder();
+    char c;
+    int state = 0, next;
 
     try {
-      while (true) {
-        curChar = (char) f.readByte();
-        if (curChar == '\n') {
-          fila++;
-          continue;
+      file.seek(lastTkLen);
+      c = (char) file.readByte();
+
+      do {
+        next = delta(state, c);
+        if (next == SKIP) {
+          if (c == '\n')
+            fila++;
+          else
+            columna++;
         }
-        columna++;
-
-        if (curChar == '(') {
-          return new Token(fila, columna, Token.PARI);
-        } else if (curChar == ')') {
-          return new Token(fila, columna, Token.PARD);
-        } else if (curChar == ':') {
-          return new Token(fila, columna, Token.DOSP);
-        } else if (curChar == '{') {
-          return new Token(fila, columna, Token.LBRA);
-        } else if (curChar == '}') {
-          return new Token(fila, columna, Token.RBRA);
-        } else if (curChar == ';') {
-          return new Token(fila, columna, Token.PYC);
+        if (next == ERROR) {
+          // Throw exception? Return null?
         }
-
-        tipo = Token.OPREL;
-        char next = (char) f.readByte();
-        if (curChar == '<') {
-          if (next == '=') {
-            return new Token(fila, columna, tipo, "<=");
-          }
-          f.seek(-1);
-          return new Token(fila, columna, tipo, "<");
-        } else if (curChar == '>') {
-          if (next == '=') {
-            return new Token(fila, columna, tipo, ">=");
-          }
-          f.seek(-1);
-          return new Token(fila, columna, tipo, ">");
-        } else if (curChar == '=') {
-          if (next == '=') {
-            return new Token(fila, columna, tipo, "==");
-          }
-          f.seek(-1);
-          return new Token(fila, columna, Token.ASIG, "=");
-        } else if (curChar == '!') {
-          next = (char) f.readByte();
-          if (next == '=') {
-            return new Token(fila, columna, tipo, "!=");
-          }
-          // TODO: continue here
-          // -----------------------------------------------------------------------------------------
+        if (next == FINAL) {
+          lastTkLen = lexbuilder.length();
+          file.seek(-lexbuilder.length());
+          return new Token(fila, columna, lastType, lexbuilder.toString());
         }
+        lexbuilder.append(c);
 
-        tipo = Token.OPAS;
-        if (curChar == '+') {
-        } else if (curChar == '-') {
-        }
-
-        tipo = Token.OPMUL;
-        if (curChar == '*') {
-        } else if (curChar == '/') {
-
-        } else if (curChar == 'c') {
-          if ((next = (char) f.readByte()) == 'l'
-              && (next = (char) f.readByte()) == 'a'
-              && (next = (char) f.readByte()) == 's'
-              && (next = (char) f.readByte()) == 's') {
-            return new Token(fila, columna, Token.CLASS);
-          }
-        } else if (curChar == 'f') {
-        } else if (curChar == 'i') {
-        } else if (curChar == 'e') {
-        } else if (curChar == 'p') {
-        }
-
-      }
+        state = next;
+        c = (char) file.readByte();
+      } while (true);
     } catch (IOException ioex) {
       return new Token(fila, columna, Token.EOF);
     }
+  }
+
+  private int delta(int state, char c) throws IOException {
+    switch (state) {
+      case 0:
+        if (c == '(')
+          lastType = Token.PARI;
+        else if (c == ')')
+          lastType = Token.PARD;
+        else if (c == ':')
+          lastType = Token.DOSP;
+        else if (c == '{')
+          lastType = Token.LBRA;
+        else if (c == '}')
+          lastType = Token.RBRA;
+        else if (c == '=') {
+          return 1;
+        } else if (c == ';')
+          lastType = Token.PYC;
+        // TODO: keep adding cases
+        else
+          return SKIP;
+        return FINAL;
+      case 1:
+        if (c == '=') {
+          lastType = Token.OPREL;
+          return 3;
+        }
+        lastType = Token.ASIG;
+        return 2;
+
+      case 2:
+      case 3:
+        file.seek(-1);
+        return FINAL;
+      default:
+        break;
+    }
+    return 0;
   }
 }
